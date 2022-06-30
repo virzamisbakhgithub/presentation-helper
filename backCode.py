cb354bf88ca2b80976e7084e9ede97f013e51ed8
virzamisbakhgithub <virzamisbakh2@gmail.com>
2022-06-29 21:48:50 -0700

    gesture detector, gesture command

--- backCode.py
+++ backCode.py
@@ -11,7 +11,7 @@ folderPath = ("slide")
 
 #Set the list images
 pathImages = sorted(os.listdir(folderPath), key=len)
-print(pathImages)
+# print(pathImages)
 
 #Camera setup
 cap = cv2.VideoCapture(0)       #parameter 0 for webcam
@@ -21,6 +21,10 @@ cap.set(4, height)
 #Variables
 imgNumber = 0
 hs, ws = int(120*1), int(180*1)
+gestureThreshold = 200
+buttonCounter = 0
+buttonPressed = False
+buttonDelay = 5                 #depend on how fast webcam read the frame
 
 #Hand detector
 detector = HandDetector(detectionCon=0.8, maxHands=1)
@@ -29,10 +33,41 @@ while True:
     #Importing images
 
     success, img = cap.read()
+    img = cv2.flip(img, 1)
     pathFullImages = os.path.join(folderPath, pathImages[imgNumber])
     imgCurrent = cv2.imread(pathFullImages)
-
+    
     hands, img = detector.findHands(img)
+    cv2.line(img, (0, gestureThreshold), (width, gestureThreshold), (0,255,0), 10)
+
+    if hands and buttonPressed is False:
+        hand = hands[0]
+        fingers = detector.fingersUp(hand)
+        cx, cy = hand['center']
+        # print(fingers)
+
+        #Gesture #1 - Left
+        if cy <= gestureThreshold:
+            if fingers == [1,0,0,0,0]:
+                print("left")
+                if imgNumber > 0:
+                    buttonPressed = True
+                    imgNumber -= 1
+
+        #Gesture #2 - Right
+        if cy <= gestureThreshold:
+            if fingers == [0,0,0,0,1]:
+                print("right")
+                if imgNumber < len(pathImages) - 1:
+                    buttonPressed = True
+                    imgNumber += 1
+
+    #Button press iteration
+    if buttonPressed:
+        buttonCounter += 1
+        if buttonCounter > buttonDelay:
+            buttonCounter = 0
+            buttonPressed = False
 
 
 
